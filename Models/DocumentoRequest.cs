02427dd7b83546dd3684b9be1b062f2fcd4b0007
nativosistemas <nativosistemas@outlook.com.ar>
2023-02-05 04:18:46 -0300

    doc03

---
+++ Models/DocumentoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DKbase.Models
+{
+    public class DocumentoRequest
+    {
+        public string documentoID { get; set; }
+        public string loginWeb { get; set; }
+    }
+}
